e0a93f6c84884e3e89557b549595411a9dfc1e4c
Yulii Lukhanin <julius.lukhanin@outlook.com>
2022-05-12 00:55:42 +0300

    Feature 1 - first commit

--- src/ua/uz/HelloApp.java
+++ src/ua/uz/HelloApp.java
@@ -5,6 +5,6 @@ public class HelloApp {
     public static void main(String[] args) {
         System.out.println("Hello world Git! ");
         System.out.println("Second commit ");
-        System.out.println("Developer branch created ");
+        System.out.println("Developer branch created " + "Feature #1");
     }
 }
